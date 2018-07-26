c130d2d61d91d6a3c233d8d821883648c0610dbc
susiantiamelia <ameliasusianti@gmail.com>
2018-07-26 15:51:01 +0700

    ganti database

--- server/app.js
+++ server/app.js
@@ -7,7 +7,7 @@ var dotenv = require('dotenv').config();
 var cors = require('cors');
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb://localhost:27017/todo_fancy', { useNewUrlParser: true });
+mongoose.connect('mongodb://amel:amel22@ds119436.mlab.com:19436/todo-fancy', { useNewUrlParser: true });
 
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
